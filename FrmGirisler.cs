b564da0bc0b27b0c0f25b66a63eb502986df0d82
Oğuzhan ÇAKMAK <83668054+oguz4han@users.noreply.github.com>
2024-09-23 11:38:43 +0300

    Add project files.

---
+++ FrmGirisler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hastane
+{
+    public partial class FrmGirisler : Form
+    {
+        public FrmGirisler()
+        {
+            InitializeComponent();
+        }
+
+        // başlangıç panel
+        // deneme
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
---
+++ FrmHastaGiris.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Hastane
+{
+    partial class FrmHastaGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.mskTc = new System.Windows.Forms.MaskedTextBox();
+            this.lnkUyeOl = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGirisYap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(52, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 53);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hasta Giriş Paneli";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(104, 140);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(140, 31);
+            this.txtSifre.TabIndex = 1;
+            // 
+            // mskTc
+            // 
+            this.mskTc.Location = new System.Drawing.Point(104, 95);
+            this.mskTc.Mask = "00000000000";
+            this.mskTc.Name = "mskTc";
+            this.mskTc.Size = new System.Drawing.Size(140, 31);
+            this.mskTc.TabIndex = 2;
+            this.mskTc.ValidatingType = typeof(int);
+            // 
+            // lnkUyeOl
+            // 
+            this.lnkUyeOl.AutoSize = true;
+            this.lnkUyeOl.Location = new System.Drawing.Point(250, 148);
+            this.lnkUyeOl.Name = "lnkUyeOl";
+            this.lnkUyeOl.Size = new System.Drawing.Size(62, 23);
+            this.lnkUyeOl.TabIndex = 3;
+            this.lnkUyeOl.TabStop = true;
+            this.lnkUyeOl.Text = "Üye Ol";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "TC No :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Şifre :";
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.Location = new System.Drawing.Point(133, 181);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(111, 33);
+            this.btnGirisYap.TabIndex = 6;
+            this.btnGirisYap.Text = "Giriş Yap";
+            this.btnGirisYap.UseVisualStyleBackColor = true;
+            // 
+            // FrmHastaGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(400, 234);
+            this.Controls.Add(this.btnGirisYap);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lnkUyeOl);
+            this.Controls.Add(this.mskTc);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.Name = "FrmHastaGiris";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmHastaGiris";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.MaskedTextBox mskTc;
+        private System.Windows.Forms.LinkLabel lnkUyeOl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGirisYap;
+    }
+}
